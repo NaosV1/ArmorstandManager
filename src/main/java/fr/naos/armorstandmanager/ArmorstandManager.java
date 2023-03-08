a42da6ad6ce1757d2e0d81727d063660c83a519e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-08 21:41:51 +0100

    Initial commit

---
+++ src/main/java/fr/naos/armorstandmanager/ArmorstandManager.java
@@ -0,0 +1,17 @@
+package fr.naos.armorstandmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ArmorstandManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/fr/naos/armorstandmanager/Metrics.java
@@ -0,0 +1,2 @@
+package fr.naos.armorstandmanager;public class Metrics {
+}
