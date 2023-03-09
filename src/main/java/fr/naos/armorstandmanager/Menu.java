8f9bf2ab9a49eda79af9138099f83f3fcbba8cf5
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 19:36:11 +0100

    adding some position

--- src/main/java/fr/naos/armorstandmanager/Menu.java
+++ src/main/java/fr/naos/armorstandmanager/Menu.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.util.EulerAngle;
 
 
 import java.util.HashMap;
@@ -42,6 +43,25 @@ public class Menu implements Listener {
         }
     }
 
+    public static void positionManage(Player player) {
+        if (!(player.hasPermission("asmanager.custominteract"))) { player.sendMessage(cData.no_perm);}
+        Inventory inventory = org.bukkit.Bukkit.createInventory(new MenuHolder(), 3 * 9, cData.title_menu);
+        posInitaliseItem(inventory);
+        player.openInventory(inventory);
+    }
+
+    private static void posInitaliseItem(Inventory inventory) {
+        inventory.setItem(10, ItemManage.createItem(false, Material.IRON_SWORD, cData.goAttack_text, 1, cData.goAttack_lore));
+        inventory.setItem(12, ItemManage.createItem(false, Material.RED_BED, cData.allonger_text, 1, cData.allonger_lore));
+        inventory.setItem(14, ItemManage.createItem(false, Material.ARROW, cData.handInAir_text, 1, cData.handInAir_lore));
+        inventory.setItem(16, ItemManage.createItem(false, Material.STONE, cData.swordInRock_text, 1, cData.swordInRock_lore));
+        for (int i = 0; i < inventory.getSize(); i++) {
+            if (inventory.getItem(i) == null) {
+                inventory.setItem(i, ItemManage.createItem(false, Material.GRAY_STAINED_GLASS_PANE, " ", 1));
+            }
+        }
+    }
+
     @EventHandler
     private void onItemClick(InventoryClickEvent event) {
         Inventory inv = event.getInventory();
@@ -56,17 +76,57 @@ public class Menu implements Listener {
                 } else if (event.getCurrentItem().getType() == Material.ENCHANTED_GOLDEN_APPLE && event.getSlot() == 13) {
                     armorstandChanged.setInvulnerable(!armorstandChanged.isInvulnerable());
                     initaliseItem(player, inv);
-                } else if (event.getCurrentItem().getType() == Material.CHAINMAIL_CHESTPLATE && event.getSlot() == 15) {
-                    player.sendMessage(ChatColor.GREEN + "IN DEV");
+                } else if (event.getCurrentItem().getType() == Material.LADDER && event.getSlot() == 15) {
+                    player.closeInventory();
+                    positionManage(player);
                 } else if (event.getCurrentItem().getType() == Material.BARRIER && event.getSlot() == 30) {
                     armorstandChanged.setGravity(!armorstandChanged.hasGravity());
                     initaliseItem(player, inv);
                 } else if (event.getCurrentItem().getType() == Material.ARMOR_STAND && event.getSlot() == 32) {
                     armorstandChanged.setBasePlate(!armorstandChanged.isVisible());
                     initaliseItem(player, inv);
+                } else if (event.getCurrentItem().getType() == Material.IRON_SWORD && event.getSlot() == 10) {
+                    armorStandPos(armorstandChanged, "goAttack");
+                } else if (event.getCurrentItem().getType() == Material.RED_BED && event.getSlot() == 12) {
+                    armorStandPos(armorstandChanged, "allonger");
+                } else if (event.getCurrentItem().getType() == Material.ARROW && event.getSlot() == 14) {
+                    armorStandPos(armorstandChanged, "handInAir");
+                } else if (event.getCurrentItem().getType() == Material.STONE && event.getSlot() == 16) {
+                    armorStandPos(armorstandChanged, "swordInRock");
                 }
                 player.setItemOnCursor(null);
             }
+        }
+    }
+
+    private void armorStandPos(ArmorStand armorStand, String position) {
+
+        armorStand.setArms(true);
+        switch (position) {
+            case "goAttack":
+                armorStand.setLeftArmPose(new EulerAngle(120, 0, 0));
+                armorStand.setLeftLegPose(new EulerAngle(120, 0, 0));
+                armorStand.setRightArmPose(new EulerAngle(280, 0, 0));
+                armorStand.setRightLegPose(new EulerAngle(-170, 0, 0));
+                break;
+            case "allonger":
+                armorStand.setLeftArmPose(new EulerAngle(164, 1, 0));
+                armorStand.setLeftLegPose(new EulerAngle(300, 0, 0));
+                armorStand.setRightArmPose(new EulerAngle(164, -1, 0));
+                armorStand.setRightLegPose(new EulerAngle(300, 0, 0));
+                break;
+            case "handInAir":
+                armorStand.setLeftArmPose(new EulerAngle(111, 0, 0));
+                armorStand.setLeftLegPose(new EulerAngle(0, 0, 0));
+                armorStand.setRightArmPose(new EulerAngle(280, 0, 0));
+                armorStand.setRightLegPose(new EulerAngle(0, 0, 0));
+                break;
+            case "swordInRock":
+                armorStand.setLeftArmPose(new EulerAngle(11, 0, 0));
+                armorStand.setLeftLegPose(new EulerAngle(0, 0, 0));
+                armorStand.setRightArmPose(new EulerAngle(280, 0, 0));
+                armorStand.setRightLegPose(new EulerAngle(-0, 0, 0));
+                break;
         }
     }
 }
--- src/main/java/fr/naos/armorstandmanager/cData.java
+++ src/main/java/fr/naos/armorstandmanager/cData.java
@@ -21,6 +21,15 @@ public class cData {
     public static String baseplate_text;
     public static String baseplate_lore_enable;
     public static String baseplate_lore_disable;
+    public static String goAttack_text;
+    public static String goAttack_lore;
+
+    public static String allonger_text;
+    public static String allonger_lore;
+    public static String handInAir_text;
+    public static String handInAir_lore;
+    public static String swordInRock_text;
+    public static String swordInRock_lore;
 
     public static String lang = ArmorstandManager.getInstance().getConfig().getString("lang");
     public static void loadConfig() {
@@ -43,6 +52,15 @@ public class cData {
             baseplate_text = "§aBase";
             baseplate_lore_enable = "§f    Cliquez pour §cdésactiver §fla base";
             baseplate_lore_disable = "§f    Cliquez pour §aactiver §fla base";
+            goAttack_text = "§aA l'attaque !";
+            goAttack_lore = "§f    Cliquez pour §emettre §fle porte armure en position attaque";
+            allonger_text = "§aAllonger";
+            allonger_lore = "§f    Cliquez pour §eallonger §fle porte armure";
+            handInAir_text = "§aMain en l'air";
+            handInAir_lore = "§f    Cliquez pour §emettre §fle porte armure en position main en l'air";
+            swordInRock_text = "§aEpée dans la roche";
+            swordInRock_lore = "§f    Cliquez pour §emettre §fle porte armure en position épée dans la roche";
+
 
         } else if (lang.equalsIgnoreCase("en")) {
 
@@ -62,6 +80,14 @@ public class cData {
             baseplate_text = "§aBase";
             baseplate_lore_enable = "§f    Click to §cdisable §fthe base";
             baseplate_lore_disable = "§f    Click to §aenable §fthe base";
+            goAttack_text = "§aGo attack !";
+            goAttack_lore = "§f    Click to §eput §fthe armorstand in attack position";
+            allonger_text = "§aStretch";
+            allonger_lore = "§f    Click to §estretch §fthe armorstand";
+            handInAir_text = "§aHand in the air";
+            handInAir_lore = "§f    Click to §eput §fthe armorstand in hand in the air position";
+            swordInRock_text = "§aSword in the rock";
+            swordInRock_lore = "§f    Click to §eput §fthe armorstand in sword in the rock position";
 
         } else {
             ArmorstandManager.getInstance().getLogger().warning("The language is not valid, please change it in the config.yml");
